4ede20f3e5fd797bb6285521dad65978332350d0
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-16 21:15:04 +0200

    refactor: Update navbar.php to change dropdown menu link text

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -50,7 +50,7 @@ $current_page = basename($_SERVER['REQUEST_URI']); // Henter filnavnet fra URL-e
 
                         <!-- Dropdown menu -->
                         <div id="dropdown-menu" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700">Your Profile</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700">Your Booking</a>
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700">Settings</a>
                             <a href="/Roombooking/public/logout.php" class="block px-4 py-2 text-sm text-gray-700">Logout</a>
                         </div>
