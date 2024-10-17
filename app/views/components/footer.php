1f825221495940a8faf7ab72397647baa599e594
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-17 19:42:14 +0200

    refactor: Update footer and padding styles in views and CSS

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer form or content goes here -->
 <footer class="text-gray-900">
-    <div class="container py-24 mx-auto text-center">
+    <div class="container pt-8 pb-2 mx-auto text-center">
         <p>&copy; <?php echo date('Y'); ?> MoteIT. All rights reserved.</p>
     </div>
 </footer>
