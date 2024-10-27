91947cdce64ff7f840839b62fd0ed190eb1bc25f
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-27 15:19:41 +0100

    mui: Add leading-normal

--- app/views/home.php
+++ app/views/home.php
@@ -9,7 +9,7 @@
 
     <main class="container mx-auto mt-6">
         <section class="text-center mb-10">
-            <h2 class="text-4xl font-bold mb-4 leading-snug bg-clip-text text-transparent bg-gradient-to-r from-[#2E3192] to-[#1BFFFF]">Simple, Efficient, Elegant</h2>
+            <h2 class="text-4xl font-bold mb-4 leading-normal bg-clip-text text-transparent bg-gradient-to-r from-[#2E3192] to-[#1BFFFF]">Simple, Efficient, Elegant</h2>
             <p class="text-gray-800 text-lg leading-snug">Experience a simple and efficient way to book your favorite room, with an elegant user experience that puts guests first.</p>
             <?php if (!isset($_SESSION['username'])): ?>
                 <a href="/Roombooking/public/login.php" class="mt-4 inline-block bg-gradient-to-r from-[#2E3192] to-[#1BFFFF] text-white font-semibold py-3 px-6 rounded-lg shadow">
