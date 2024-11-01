15fc2f766ce42c7f880a19b638d8d9a7161cb045
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-11-01 15:44:51 +0100

    mui: Adjust layout for adults and children input fields to use full width

--- app/views/search.php
+++ app/views/search.php
@@ -28,7 +28,7 @@
                     </div>
                     <div>
                         <label for="adults" class="block text-sm font-medium text-gray-700 mb-2">Adults</label>
-                        <div class="relative flex items-center max-w-[10rem]">
+                        <div class="relative flex items-center w-full">
                             <button type="button" id="decrement-adults" data-input-counter-decrement="adults" class="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-l-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none">
                                 <svg class="w-3 h-3 text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
@@ -44,7 +44,7 @@
                     </div>
                     <div>
                         <label for="children" class="block text-sm font-medium text-gray-700 mb-2">Children</label>
-                        <div class="relative flex items-center max-w-[10rem]">
+                        <div class="relative flex items-center w-full">
                             <button type="button" id="decrement-children" data-input-counter-decrement="children" class="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-l-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none">
                                 <svg class="w-3 h-3 text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
